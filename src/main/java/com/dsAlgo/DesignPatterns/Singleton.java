dfde31d139a4af627e4890cb1d0427ae8902b139
lavinaa <agrawallavina45@gmail.com>
2025-02-06 15:19:55 +0530

    Singleton Design Pattern

---
+++ src/main/java/com/dsAlgo/DesignPatterns/Singleton.java
@@ -0,0 +1,27 @@
+package com.dsAlgo.DesignPatterns;
+
+public class Singleton {
+    private static Singleton obj;
+// data member is static so that memory is allocated only once
+    private Singleton(){
+        System.out.println("Singleton is instantiated");
+    }
+
+    public static Singleton getInstance(){
+       if(obj == null){
+           obj = new Singleton();
+       }
+
+        return obj;
+    }
+
+    public static void doSomething(){
+        System.out.println("Something is done");
+    }
+
+    public static void main(String[] args) {
+        Singleton.getInstance().doSomething();
+    }
+
+}
+
